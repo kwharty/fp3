e94dfaf4895e1f66e2001ec73389738d1636eeb2
Norton Nice <nortonnice@gmail.com>
2019-01-15 10:09:32 -0800

    commiting local changes to pull master

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Tier2 from "./components/Tier2";
-
+import Navbar from "./components/Navbar"
 
 
 
@@ -15,11 +15,12 @@ class App extends Component {
     return (
       <div>
       <Tier2 />
+      <Navbar />
 
       <div className="masthead container d-flex h-100 align-items-center">
       <div className="mx-auto text-center">
         <h1 className="mx-auto my-0 text-uppercase headerAutoBongs ">AutoBongs</h1>
-        <h2 className="text-white-50 mx-auto mt-2 mb-5">The Ultimate Monthly ######### for Glass Collectors </h2>
+        <h2 className="text-white-50 mx-auto mt-2 mb-5">The Ultimate Monthly Subscription for Glass Collectors </h2>
         <a href="pages/login.html" class="btn btn-primary js-scroll-trigger ">Get Started</a>
       </div>
     </div>
--- src/components/Tier2/index.js
+++ src/components/Tier2/index.js
@@ -1,79 +1,82 @@
 import React from 'react';
 
 
-const Tier2 = () =>  {
 
-//   /* <div class="row bg-black justify-content-center no-gutters mb-5 mb-lg-0">
-// <div class="col-lg-6">
-//   <!-- <img class="img-fluid" src="img/bong2.jpg" alt=""> -->
-//   <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-//     <ol class="carousel-indicators">
-//       <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-//       <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-//       <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-//     </ol>
-//     <div class="carousel-inner">
-//       <div class="carousel-item active">
-//         <img class="d-block w-100" src="./img/bong1.jpg" alt="glass">
-//       </div>
-//       <div class="carousel-item">
-//         <img class="d-block w-100" src="./img/ashCatch1.jpg" alt="Accoriess">
-//       </div>
-//       <div class="carousel-item">
-//         <img class="d-block w-100" src="./img/bowl1.jpg" alt="swag">
-//       </div>
-//     </div>
-//     <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-//       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-//       <span class="sr-only">Previous</span>
-//     </a>
-//     <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-//       <span class="carousel-control-next-icon" aria-hidden="true"></span>
-//       <span class="sr-only">Next</span>
-//     </a>
-//   </div>
+const Tier2 = () => {
+
+  <div className="row bg-black justify-content-center no-gutters mb-5 mb-lg-0">
+    <div className="col-lg-6">
+
+      <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+        <ol className="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img className="d-block w-100" src="./img/bong1.jpg" alt="glass" />
+          </div>
+          <div className="carousel-item">
+            <img className="d-block w-100" src="./img/ashCatch1.jpg" alt="Accoriess" />
+          </div>
+          <div className="carousel-item">
+            <img className="d-block w-100" src="./img/bowl1.jpg" alt="swag" />
+          </div>
+        </div>
+        <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="sr-only">Previous</span>
+        </a>
+        <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="sr-only">Next</span>
+        </a>
+        {/* </div> */}
 
 
-// </div>
-// <div class="col-lg-6">
-//   <div class="bg-black text-center h-100 project">
-//     <div class="d-flex h-100">
-//       <div class="project-text w-100 my-auto text-center text-lg-left">
-//         <div class="card mb-4 box-shadow">
-//           <div class="card-header">
-//             <h2 class="my-0 font-weight-normal">Teir 2</h2>
-//           </div>
-//           <div class="card-body">
-//             <h1 class="card-title pricing-card-title">$50 <small class="text-muted">/ mo</small></h1>
-//             <ul class="list-unstyled mt-3 mb-4">
-//               <li>or a single box $65</li><br>
-//               <li>Shipped to you in discrete packaging.</li><br>
-//               <li>Collectible box</li><br>
-//               <li>An even higher quaility water pipe with accesorries</li>
-//               <li> add a teir 1 combo for only $25</li><br>
-//               <li> 2x Raffle entry for giveaways.</li>
-//               <br>
-//               <li>A $110 Value!</li>
-//             </ul>
-//             <button type="button" class="btn btn-md btn-block btn-primary"><a href="pages/login.html">
-//                 <h4 class="text-white"> Get started</h4>
-//               </a></button>
-//           </div>
-//         </div>
-//         <hr class="d-none d-lg-block mb-0 ml-0">
-//       </div>
-//     </div>
-//   </div>
-// </div>
-// </div> */
-    return (
+        {/* </div> */}
+        <div className="col-lg-6">
+          <div className="bg-black text-center h-100 project">
+            <div className="d-flex h-100">
+              <div className="project-text w-100 my-auto text-center text-lg-left">
+                <div className="card mb-4 box-shadow">
+                  <div className="card-header">
+                    <h2 className="my-0 font-weight-normal">Teir 2</h2>
+                  </div>
+                  <div className="card-body">
+                    <h1 className="card-title pricing-card-title">$50 <small className="text-muted">/ mo</small></h1>
+                    <ul className="list-unstyled mt-3 mb-4">
+                      <li>or a single box $65</li>
+                      <li>Shipped to you in discrete packaging.</li>
+                      <li>Collectible box</li>
+                      <li>An even higher quaility water pipe with accesorries</li>
+                      <li> add a teir 1 combo for only $25</li>
+                      <li> 2x Raffle entry for giveaways.</li>
+
+                      <li>A $110 Value!</li>
+                      {/* </ul> */}
+                      <button type="button" className="btn btn-md btn-block btn-primary"><a href="pages/login.html">
+                        <h4 className="text-white"> Get started</h4>
+                      </a></button>
+                      </div>
+                      </div>
+                      <hr className="d-none d-lg-block mb-0 ml-0">
+                        </div>
+                        </div>
+
+                        </div>
+                        </div>
+                        return (
         <div>
-<p>TESTING RENDER</p>
-</div>
-
-    )
-}
-
-export default Tier2;
+                          <p>TESTING RENDER</p>
+                        </div>
 
 
+
+                        )
+                    }
+                    
+                    export default Tier2;
+                    
+                    
