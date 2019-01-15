5fc951617c33cde4b466359e78cadc1228b2a72c
Norton Nice <nortonnice@gmail.com>
2019-01-14 20:20:33 -0800

    changed render function in index.js to render: App.js instead of about, added component Tier2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Tier2 from "./components/Tier2";
 
 
 
@@ -12,17 +13,17 @@ import './App.css';
 class App extends Component { 
   render() {
     return (
-        
-
+      <div>
+      <Tier2 />
 
       <div className="masthead container d-flex h-100 align-items-center">
       <div className="mx-auto text-center">
         <h1 className="mx-auto my-0 text-uppercase headerAutoBongs ">AutoBongs</h1>
-        <h2 className="text-white-50 mx-auto mt-2 mb-5">The Ultimate Monthly Subscription for Glass Collectors </h2>
+        <h2 className="text-white-50 mx-auto mt-2 mb-5">The Ultimate Monthly ######### for Glass Collectors </h2>
         <a href="pages/login.html" class="btn btn-primary js-scroll-trigger ">Get Started</a>
       </div>
     </div>
-
+    </div>
 
 
 
