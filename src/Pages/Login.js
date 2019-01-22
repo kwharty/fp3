70548c4f3fbedaa51517c014972dea0f74992128
Norton Nice <nortonnice@gmail.com>
2019-01-22 12:01:01 -0800

    login in navbar, create account page/link, need to style more

---
+++ src/Pages/Login.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Login from "../components/Login/Login"
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+
+
+const Login = () => {
+
+    return (
+        <div>
+            <Navbar />
+            <Login />
+            <Footer />
+
+        </div>
+    );
+
+}
+
+export default Login;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./about.css";
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
@@ -7,7 +8,13 @@ function Header() {
       <div className="mx-auto text-center">
         <h1 className="mx-auto my-0 text-uppercase headerAutoBongs ">AutoBongs</h1>
         <h2 className="text-white-50 mx-auto mt-2 mb-5">The Ultimate Monthly Subscription for Glass Collectors </h2>
-        <a href="pages/login.html" class="btn btn-primary js-scroll-trigger ">Get Started</a>
+        <Link
+          to="/login">
+          <button type="button"
+          className="btn btn-primary">
+          Create New Account
+          </button>
+            </Link>
       </div>
     </div>
   );
