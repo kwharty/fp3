572269694a60e477697d6d47b7397c5d19354c87
philiplross <philiplross@gmail.com>
2019-02-02 13:59:14 -0800

    css work

--- client/src/components/DeveloperCard/index.js
+++ client/src/components/DeveloperCard/index.js
@@ -7,21 +7,38 @@ const DeveloperCard = props => {
 
 
 
-        <div className="card">
+        <div className="card" style ={{
+          backgroundColor: "grey",
+          backgroundImage: 'radial-gradient(white, grey, black)',
+
+
+        }}  >
 
 
 
-      <div className="img-container">
-        <img alt={props.name} src={props.image} style ={{
+      <div className="img-container ">
+        <img  className="allign-center" alt={props.name} src={props.image} style ={{
 
 width: "30%",
-height: "30%"
+height: "30%",
+alignContent: "center",
+display: "block",
+marginLeft: "auto",
+marginRight: "auto",
+
 
 
         }}/>
       </div>
-      <div className="content">
-        <ul>
+      <div className="content text-center" style ={{
+// backgroundColor: "grey",
+color: "white",
+fontSize: "25px",
+textShadow: "2px 2px 4px #000000",
+
+
+      }} >
+        <ul >
           <li>
             <strong>Name:</strong> {props.name}
           </li>
