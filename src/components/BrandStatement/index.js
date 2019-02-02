9a514a0c3bfdf9999e5caf625f9501f602cc1dc2
philiplross <philiplross@gmail.com>
2019-02-02 10:21:51 -0800

    front end changes

--- src/components/BrandStatement/index.js
+++ src/components/BrandStatement/index.js
@@ -7,7 +7,7 @@ const BrandStatement = () => {
             color: 'white',
             fontSize: '25px',
             textShadow: '5px 5px 8px #000000',
-
+            
 
         }}>
             <br></br>
@@ -20,6 +20,26 @@ const BrandStatement = () => {
 
             The service is comprised of 3 tiers of increasing quality and price. Each tier is curated according to your preferences, and allows us to build your perfect bong taste.
             After signing up and filling out our survey, we personaly select you peice hand picked by one of our Autobong curiators.
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <h1 style={{
+fontSize: '60px',
+textAlign: 'center',
+textShadow: '2px 2px 5px black',
+ 
+
+
+}} >Meet the creators of Autobongs</h1>
+
+<hr style={{
+    
+}} ></hr>
+    
+    
+    
     </div>
     );
 }
--- src/components/DeveloperCard/index.js
+++ src/components/DeveloperCard/index.js
@@ -2,7 +2,14 @@ import React from "react";
 
 const DeveloperCard = props => {
     return (
+
+
+
+
         <div className="card">
+
+
+
       <div className="img-container">
         <img alt={props.name} src={props.image} />
       </div>
