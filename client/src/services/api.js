162cb5686461ed19b7cc72272b184bcdc5aac6cf
Irvin Lee Nelson <irvinlnelson@Irvins-MacBook-Pro.local>
2019-02-04 18:29:08 -0800

    pull

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,5 +4,9 @@ export default {
     register: (userInfo) => {
         return axios.post("/api/users", userInfo)
             .then(newUser => newUser.data)
+    },
+    login: (userInfo) => {
+        return axios.get("/api/users", userInfo)
+        .then(userLogin => userLogin.data)
     }
 }
