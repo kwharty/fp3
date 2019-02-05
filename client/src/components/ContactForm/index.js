675460bb918d853e4708ba7f1a0045e728ac0712
Norton Nice <nortonnice@gmail.com>
2019-02-05 13:16:59 -0800

    reverted to working conditional rendering, styling may need to be redone on some components

--- client/src/components/ContactForm/index.js
+++ client/src/components/ContactForm/index.js
@@ -4,54 +4,39 @@ import "./form.css";
 
 const ContactForm = () => {
     return (
-
-        <div className="container-fluid">
-            <form  className="col-8" style={{
-                    margin: '50px',
-                    alignContent: "left",
-
-
-                    
-                }}>
-                <div className="form-group col-12" style={{
-                    //  marginLeft: '0%',
-                    alignContent: "left",
-
-                }} >
-                    <label for="exampleFormControlInput1">Name</label>
-                    <input type="email" className="form-control alig" id="exampleFormControlInput1" placeholder="Michael Jordan"></input>
-                </div>
-
-
-                <div className="form-group col-12" style={{
-                    //  marginLeft: '0%',
-                    alignContent: "left",
-
-
-
-                }}>
-                    <label for="exampleFormControlInput1">Email address</label>
-                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"></input>
-                </div>
-
-
-                <div className="form-group col-12" style={{
-                    //  marginLeft: '0%',
-
-
-
-                }}>
-                    <label for="exampleFormControlTextarea1">Leave Your Message Below</label>
-                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
-            
+        // <div className="jumbotron jumbotron-fluid">
+        <div className="container" id="formContainer">
+            <div className="title" id="contactTitle">
+                <h1>Contact Us</h1>
+                <p className="lead">Please enter your information below.</p>
             </div>
-         
- 
+            <form className="form" id="infoBucket">
+                <div className="newFormGroup">
+                    <label className="newFormLabel">Name</label>
+                    <input type="text" className="formControl" id="formControlInput1" placeholder="Enter Name"></input>
+                </div>
+                <div className="newFormGroup">
+                    <label className="newFormLabel">Email address</label>
+                    <input type="email" className="formControl" id="formControlInput2" placeholder="Email Here"></input>
+                </div>
+                <div className="newFormGroup">
+                    <label className="newFormLabel">Message</label>
+                    <textarea type="text" className="formControl" id="formControlInput3" placeholder="Your Message Here"></textarea>
+                </div>
+                <div className="submitButton">
+                    <input type="submit" value="submit" />
+                </div>
+            </form>
+        </div>
+        // </div>
 
-    );
+    )
 }
 
 export default ContactForm;
+
+
+
+
+
+
