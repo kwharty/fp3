4364874c9fb22c98f05e06375faab3a420536a22
Norton Nice <nortonnice@gmail.com>
2019-02-05 16:21:42 -0800

    finished styling contact form

--- client/src/components/ContactForm/index.js
+++ client/src/components/ContactForm/index.js
@@ -12,19 +12,27 @@ const ContactForm = () => {
             </div>
             <form className="form" id="infoBucket">
                 <div className="newFormGroup">
-                    <label className="newFormLabel">Name</label>
+                    <div className="row">
+                        <label className="newFormLabel">Name</label>
+                    </div>
                     <input type="text" className="formControl" id="formControlInput1" placeholder="Enter Name"></input>
                 </div>
                 <div className="newFormGroup">
-                    <label className="newFormLabel">Email address</label>
+                    <div className="row">
+                        <label className="newFormLabel">Email address</label>
+                    </div>
                     <input type="email" className="formControl" id="formControlInput2" placeholder="Email Here"></input>
                 </div>
                 <div className="newFormGroup">
-                    <label className="newFormLabel">Message</label>
+                    <div className="row">
+                        <label className="newFormLabel">Message</label>
+                    </div>
                     <textarea type="text" className="formControl" id="formControlInput3" placeholder="Your Message Here"></textarea>
                 </div>
                 <div className="submitButton">
-                    <input type="submit" value="submit" />
+                    <div className="row">
+                        <input type="submit" value="submit" id="submitButton" />
+                    </div>
                 </div>
             </form>
         </div>
